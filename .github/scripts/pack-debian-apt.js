990fa032fb910aff6baf7939e1a8798fb35d16f1
twilio-dx <team_interfaces+github@twilio.com>
2022-10-05 17:00:34 +0530

    changed forward slash

--- .github/scripts/pack-debian-apt.js
+++ .github/scripts/pack-debian-apt.js
@@ -13,7 +13,7 @@ const git_commit_script = {
   get : (version) => `git config --global user.email "rohithprakashklm@gmail.com"
 git config --global user.name "rohith-prakash"
 branch=$(git branch --show-current)
-git add apt/
+git add apt
 if [ -n "$(git status --porcelain)" ]; then
   echo "There are changes to commit.";
   git commit -m "Release ${version}"
